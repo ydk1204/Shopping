a45c99de870221c25a03c6038a3d278f7c56e69e
ydk1204 <ejrbdi@naver.com>
2021-06-30 20:38:59 +0900

    Javascript change

--- Vanila.js
+++ Vanila.js
@@ -1,3 +1,5 @@
+//JS만을 이용해서 쇼핑 사이트 관리
+
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
@@ -20,21 +22,32 @@ function onAdd() {
     input.focus();
 }
 
-let id = 0; //UUID
 function creatItem(text) {
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
-    itemRow.setAttribute('data-id', id)
-    itemRow.innerHTML = `
-        <div class="item">
-            <span class="item__name">${text}</span>
-                <button class="item__delete">
-                    <i class="fas fa-trash-alt" data-id=${id}></i>
-                </button>
-            </div>
-        <div class="item__divider"></div>
-    `;
-    id++;
+
+    const item = document.createElement('div');
+    item.setAttribute('class', 'item');
+
+    const name = document.createElement('span');
+    name.setAttribute('class', 'item__name');
+    name.innerText = text;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('class', 'item__delete');
+    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    deleteBtn.addEventListener('click', ()=>{
+        items.removeChild(itemRow);
+    })
+
+    const itemDivider = document.createElement('div');
+    itemDivider.setAttribute('class', 'item__divider');
+
+    item.appendChild(name);
+    item.appendChild(deleteBtn);
+
+    itemRow.appendChild(item);
+    itemRow.appendChild(itemDivider);
     return itemRow;
 }
 
@@ -45,13 +58,5 @@ addBtn.addEventListener('click', ()=>{
 input.addEventListener('keypress', (event)=>{
     if(event.key === 'Enter') {
         onAdd();
-    }
-})
-
-items.addEventListener('click', event => {
-    const id = event.target.dataset.id;
-    if(id) {
-        const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
-        toBeDeleted.remove();
     }
 })
